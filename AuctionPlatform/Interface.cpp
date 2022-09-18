09653e7b8e2179109c33456fab48cd314bb132cf
Esther <1326121237@qq.com>
2022-09-19 01:20:04 +0800

    add file

---
+++ AuctionPlatform/Interface.cpp
@@ -0,0 +1,8 @@
+//
+//  Interface.cpp
+//  AuctionPlatform
+//
+//  Created by 陈雨欣 on 2022/9/19.
+//
+
+#include "Interface.h"
---
+++ AuctionPlatform/Interface.h
@@ -0,0 +1,13 @@
+//
+//  Interface.h
+//  AuctionPlatform
+//
+//  Created by 陈雨欣 on 2022/9/19.
+//
+
+#ifndef Interface_h
+#define Interface_h
+
+#include <stdio.h>
+
+#endif /* Interface_hpp */
